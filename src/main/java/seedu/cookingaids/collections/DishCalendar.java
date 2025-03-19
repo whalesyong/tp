b18f0d359fe25ba1cc4a4202e2cedc452c964ce3
Jeromy <56627060+jxromy@users.noreply.github.com>
2025-03-19 12:55:17 +0800

    Added JUnit tests for delete dish, ingredients and recipes

--- src/main/java/seedu/cookingaids/collections/DishCalendar.java
+++ src/main/java/seedu/cookingaids/collections/DishCalendar.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 
 public class DishCalendar {
-    private static ArrayList<Dish> dishCalendar = new ArrayList<>();
+    public static ArrayList<Dish> dishCalendar = new ArrayList<>();
 
     //load data from JSON when first accessed
     public static void initializeDishCalendar(List<Dish> dishes) {
@@ -73,4 +73,16 @@ public class DishCalendar {
         dishCalendar.remove(dishesToBeRemoved);
     }
 
+    public boolean containsDish(String dishName) {
+        for (Dish dish : dishCalendar) {
+            if (dish.getName().equalsIgnoreCase(dishName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void clear() {
+        dishCalendar.clear();
+    }
 }
