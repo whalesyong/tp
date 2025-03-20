3d99da8a8188d97e583ee3a070d4f8bd57444ccd
Jeromy <56627060+jxromy@users.noreply.github.com>
2025-03-20 17:01:37 +0800

    Fixed checkstyle errors

--- src/test/java/seedu/cookingaids/items/IngredientTest.java
+++ src/test/java/seedu/cookingaids/items/IngredientTest.java
@@ -2,7 +2,7 @@ package seedu.cookingaids.items;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IngredientTest {
 
