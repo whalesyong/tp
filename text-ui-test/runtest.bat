824cb15701d839ea66e5300e737a4f97243b64a8
Jeromy <56627060+jxromy@users.noreply.github.com>
2025-03-29 11:09:15 +0800

    Reverting change

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -15,5 +15,4 @@ for /f "tokens=*" %%a in (
 java -jar %jarloc% < ..\..\text-ui-test\input.txt > ..\..\text-ui-test\ACTUAL.TXT
 
 cd ..\..\text-ui-test
-
 fc /w  ACTUAL.TXT EXPECTED.TXT >NUL && ECHO Test passed! || Echo Test failed!
